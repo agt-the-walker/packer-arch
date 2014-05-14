4778d86d8879442db20588c7a87ea0acf0d9d886
Jerome Zago <jerome.zago@gmail.com>
2014-05-14 15:07:12 +0200

    Create another, unformatted, partition

--- install-virtualbox.sh
+++ install-virtualbox.sh
@@ -18,8 +18,9 @@ echo "==> destroying magic strings and signatures on ${DISK}"
 /usr/bin/dd if=/dev/zero of=${DISK} bs=512 count=2048
 /usr/bin/wipefs --all ${DISK}
 
-echo "==> creating /root partition on ${DISK}"
-/usr/bin/sgdisk --new=1:0:0 ${DISK}
+echo "==> creating /root and an extra partition on ${DISK}"
+/usr/bin/sgdisk --new=1:0:15G ${DISK}
+/usr/bin/sgdisk --new=2:0:0 ${DISK}
 
 echo "==> setting ${DISK} bootable"
 /usr/bin/sgdisk ${DISK} --attributes=1:set:2
